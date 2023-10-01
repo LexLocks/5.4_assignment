d8cd379d6b8a1922c5d3e75bb03e71620751f7bf
Alexandra Coleman <97180460+LexLocks@users.noreply.github.com>
2023-10-01 05:43:32 -0500

    2nd

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-const text = document.querySelector (".title");
+// Using a for loop to generate a list of items
+var itemList = document.getElementById("item-list");
 
-const changeColor = document.querySelector (".changeColor");
+for (var i = 1; i <= 5; i++) {
+    var listItem = document.createElement("li");
+    listItem.textContent = "Item " + i;
+    itemList.appendChild(listItem);
+}
 
-text.classList.add("change");
+// Using a while loop to update CSS based on a variable value
+var changeColorButton = document.getElementById("changeColorButton");
+var backgroundColor = "lightblue";
+var colorChangeFlag = false;
+
+changeColorButton.addEventListener("click", function() {
+    // Toggle the color between lightblue and lightcoral
+    if (colorChangeFlag) {
+        backgroundColor = "lightblue";
+    } else {
+        backgroundColor = "lightcoral";
+    }
+    
+    // Update CSS styles based on the variable value
+    itemList.style.backgroundColor = backgroundColor;
+    colorChangeFlag = !colorChangeFlag;
+});
